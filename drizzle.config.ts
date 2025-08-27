0663710d364000bc56d17955e1e377a77526b3f0
vedri45 <vedri45@gmail.com>
2025-08-27 14:57:13 +0700

    Update drizzle config with Turso database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.TURSO_CONNECTION_URL) throw new Error('TURSO_CONNECTION_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: { url: process.env.TURSO_CONNECTION_URL },
 	verbose: true,
 	strict: true
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,8 @@ import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.TURSO_CONNECTION_URL || !env.TURSO_AUTH_TOKEN) throw new Error('TURSO_CONNECTION_URL or TURSO_AUTH_TOKEN is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({ url: env.TURSO_CONNECTION_URL, authToken: env.TURSO_AUTH_TOKEN });
 
 export const db = drizzle(client, { schema });
